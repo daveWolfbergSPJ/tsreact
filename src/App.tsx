a34ad7474e3ae0f7bc2819b82ecc1e1547ac55bb
Dave Wolfberg <davewolfberg@Daves-MacBook-Air.local>
2022-07-18 08:47:49 -0700

    click handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { FC } from "react";
 import "./App.css";
 
-const Heading = ({title}: { title: string}) => <h2>{title}</h2>;
-const Box = ({children}: { children: React.ReactNode}) => (
-  <div>
+interface BoxProps {
+  children?: React.ReactNode;
+}
+
+const Heading = ({ title }: { title: string }) => <h2>{title}</h2>;
+
+const Box: FC<BoxProps> = ({ children }) => (
+  <div
+    style={{
+      padding: "1rem",
+      fontWeight: "bold",
+    }}
+  >
     {children}
   </div>
-) 
+);
+
+const List: FC<{ 
+  items: string[];
+  onClick?: (item: string) => void
+ }> = ({ items, onClick }) => (
+  <ul>
+    {items.map((item, index) => (
+      <li key={index} onClick={() => onClick?.(item)}>{item}</li>
+    ))}
+  </ul>
+);
 
 function App() {
+  const onListClick = useCallback((item: string) => {
+    alert(item)
+  }, []);
   return (
     <div>
       <Heading title="TSREACT" />
-      <Box>
-        Hello There
-      </Box>
+      <Box>Hello There!</Box>
+      <List items={['one', 'two', 'three']} onClick={onListClick}/>
     </div>
   );
 }
